ca2de55fe0adba552f03fb9593067f20da5ac288
Tyler Cozzocrea <tylercozzocrea@gmail.com>
2024-10-07 11:11:23 -0700

    Delete unnecessary puts statements

---
+++ main.rb
@@ -0,0 +1,33 @@
+# Name Mangler
+
+@name = "Johanna Jackson"
+
+# Mangle the name by reversing it or borgifying it
+def modified_name(borgify)
+  # Split the name into first and last in an array
+  split_name = @name.split
+  #reversed_twice = split_name.reverse.reverse
+  #mangled_name = reversed_twice << "Mangled"
+  #return mangled_name * 2
+  # Reverse the array of name components so the array is [last, first]
+  reversed_name_components = split_name.reverse
+  # Rejoin the array of name components
+  new_name1 = reversed_name_components.join(' ')
+  # Store the mangled name
+  @name = new_name1
+  if borgify
+    # Split the name into last and first in an array
+    split_name2 = new_name1.split
+    # Reverse the array of name components so the array is [last, first]
+    reversed_name_components2 = split_name2.reverse
+    # Add borg to the end of the array
+    reversed_name_components2 << "Borg"
+    # Rejoin the array of name components
+    new_name2 = reversed_name_components2.join(' ')
+    @name = new_name2
+  end
+  return @name
+end
+
+puts "Reversed Name: #{modified_name(false)}"
+puts "Borgified Name: #{modified_name(true)}"
